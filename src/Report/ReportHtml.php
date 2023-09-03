95b2cdae2eca63a0bc35bc49f7877c9957ada9b3
Tilson Mateus <tilsonmat@gmail.com>
2023-09-03 21:40:15 +0100

    feat(reportHTML/stubs): create stubs for report HTML

--- src/Report/ReportHtml.php
+++ src/Report/ReportHtml.php
@@ -2,7 +2,9 @@
 
 namespace Tilson\GitReportPhp\Report;
 
+use Illuminate\Filesystem\Filesystem;
 use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\File;
 use Tilson\GitReportPhp\Contract\ReportContract;
 use Tilson\GitReportPhp\GitLogCommand;
 
@@ -24,20 +26,47 @@ final class ReportHtml implements ReportContract
     private array $matches = [];
 
     /**
+     * @param int $totalCommits
+     */
+    private int $totalCommits = 0;
+
+    /**
+     * @param Collection $totalCommits
+     * @description Set total commits
+     */
+    private function setTotalCommits(Collection $totalCommits)
+    {
+        $this->totalCommits = $totalCommits->count();
+    }
+
+    private function createFolderAndCopyStubsFiles()
+    {
+        // $filesystem = new Filesystem();
+
+        // $filesystem->makeDirectory(resource_path('report'), 0755, true);
+
+        File::get(__DIR__ . '/../../stubs/report_stub');
+    
+    }
+
+    /**
      * Generate report from git log     
-     * 
-     * */
+     */
     public function generateReport(array $options)
     {
         $allCommits = app(GitLogCommand::class)->exec();
-        
+
         $allCommits->each(function ($commit) {
             if (preg_match($this->pattern, substr($commit, 0, 10), $this->matches)) {
                 array_push($this->commits, $this->matches[1]);
             }
         });
 
+        $this->setTotalCommits($allCommits);
+        dd($allCommits->pluck(''));
+        $this->createFolderAndCopyStubsFiles();
+        
 
-         dd($this->commits, $allCommits);
+
     }
 }
